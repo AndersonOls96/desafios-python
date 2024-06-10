9d174f7bca006341b2f5b099661f2b39645437f1
Ketlenx <167771419+Ketlenx@users.noreply.github.com>
2024-06-10 11:24:25 -0300

    Update CadCliente.py

--- exercises/CadCliente.py
+++ exercises/CadCliente.py
@@ -3,17 +3,9 @@ class Cliente:
         self.nome = nome
         self.telefone = telefone
         self.email = email
-
+ 
     def exibir_informacoes(self):
+        # Implemente o método exibir_informacoes
         return f"Nome: {self.nome}\nTelefone: {self.telefone}\nEmail: {self.email}"
-
-# Exemplo de uso:
-cliente1 = Cliente("Ketlen", "123456789", "Ketlen@example.com")
-print(cliente1.exibir_informacoes())
-
-# Criando outro cliente
-cliente2 = Cliente("Andersson", "987654321", "Andersson@example.com")
-
-# Exibindo as informações do segundo cliente
-print(cliente2.exibir_informacoes())
+    
     
