a0ed099793a6c583c6c3465c23a6d2745346f50c
Ketlenx <167771419+Ketlenx@users.noreply.github.com>
2024-06-10 10:46:18 -0300

    Update IndiceAlfabeto.py

--- exercises/IndiceAlfabeto.py
+++ exercises/IndiceAlfabeto.py
@@ -1,11 +1,21 @@
 class Alfabeto:
     def __init__(self):
-        # Adicione os atributos necessários
-        # Dica: você pode adicionar as letras do alfabeto em uma lista
-        pass # Remova esta linha
+        # Criando uma lista com as letras do alfabeto
+        self.letras = [chr(i) for i in range(97, 123)]
 
     def indice_para_letra(self, indice):
         if 1 <= indice <= 26:
-            return # Implemente o método indice_para_letra
+            return self.letras[indice - 1]
         else:
             return ''
+
+# Testes
+alfabeto = Alfabeto()
+
+# Teste para índices válidos
+for i in range(1, 27):
+    print(f"Letra correspondente ao índice {i}: {alfabeto.indice_para_letra(i)}")
+
+# Teste para índices inválidos
+print("Letra correspondente ao índice 0:", alfabeto.indice_para_letra(0))
+print("Letra correspondente ao índice 27:", alfabeto.indice_para_letra(27))
