66285b2510708d12865e59a8ffacca836fb39fd4
Ketlenx <167771419+Ketlenx@users.noreply.github.com>
2024-06-10 11:24:03 -0300

    Update AumentoPercentual.py

--- exercises/AumentoPercentual.py
+++ exercises/AumentoPercentual.py
@@ -1,15 +1,12 @@
 class FormatarPercentual:
     def __init__(self):
-        self.aumento_vendas = 0
-
+        # Adicione o(s) atributo(s) necessário(s)
+        self.aumento_vendas = 0.0
+ 
     def definir_aumento_vendas(self, aumento):
+        # Implemente o método definir_aumento_vendas com o(s) parâmetro(s) e atribuição(ões) necessária(s)
         self.aumento_vendas = aumento
-
+ 
     def exibir_aumento_formatado(self):
-        aumento_formatado = "{:.2f}%".format(self.aumento_vendas)
-        return aumento_formatado
-    
-# Teste da classe
-formatador = FormatarPercentual()
-formatador.definir_aumento_vendas(8.9)
-print(formatador.exibir_aumento_formatado())  # Saída esperada: 8.90%
+        aumento_formatado = "{:.2f}".format(self.aumento_vendas)
+        return f"O aumento percentual de vendas foi de {aumento_formatado}%"
